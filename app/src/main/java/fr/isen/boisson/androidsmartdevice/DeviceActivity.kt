0a0f8d2b823866e094530925ed642518414fb748
Lou13213 <67229851+Lou13213@users.noreply.github.com>
2023-03-28 16:59:56 +0200

    28/03 16h59

--- app/src/main/java/fr/isen/boisson/androidsmartdevice/DeviceActivity.kt
+++ app/src/main/java/fr/isen/boisson/androidsmartdevice/DeviceActivity.kt
@@ -1,11 +1,49 @@
 package fr.isen.boisson.androidsmartdevice
 
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothProfile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.core.view.isVisible
+import fr.isen.boisson.androidsmartdevice.databinding.ActivityDeviceBinding
+@SuppressLint("MissingPermission")
 class DeviceActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDeviceBinding
+    private var bluetoothGatt: BluetoothGatt? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_device)
+        binding = ActivityDeviceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bluetoothDevice: BluetoothDevice? = intent.getParcelableExtra("device")
+        val bluetoothGatt = bluetoothDevice?.connectGatt(this, false, bluetoothGattCallback)
+        bluetoothGatt?.connect()
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        bluetoothGatt?.close()
+    }
+
+    private val bluetoothGattCallback = object : BluetoothGattCallback() {
+        override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                runOnUiThread {
+                    displayContentConnected()
+                }
+            }
+        }
+    }
+
+    private fun displayContentConnected() {
+        binding.detailLoaderTitle.text = getString(R.string.device_led_text)
+        binding.detailLoader.isVisible = true
+        binding.led1.isVisible = true
     }
 }
