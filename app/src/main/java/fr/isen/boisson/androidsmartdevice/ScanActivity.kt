59e57c8976973ca2a06fcaf11993c04bda28bcb2
Lou13213 <67229851+Lou13213@users.noreply.github.com>
2023-03-28 14:10:13 +0200

    28/03 14h09

--- app/src/main/java/fr/isen/boisson/androidsmartdevice/ScanActivity.kt
+++ app/src/main/java/fr/isen/boisson/androidsmartdevice/ScanActivity.kt
@@ -1,46 +1,195 @@
 package fr.isen.boisson.androidsmartdevice
 
+import android.bluetooth.BluetoothAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.Manifest
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import fr.isen.boisson.androidsmartdevice.databinding.ActivityScanBinding
 
 class ScanActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityScanBinding
 
-    private var mScanning = false
+    private val bluetoothAdapter: BluetoothAdapter? by
+    lazy(LazyThreadSafetyMode.NONE) {
+        val bluetoothManager =
+            getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+    private var mscanning = false
+    private val handler = Handler(Looper.getMainLooper())
+    private lateinit var adapter: ScanAdapter
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            if (permissions.all { it.value }) {
+                scanBLEDevices()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityScanBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (bluetoothAdapter == null) {
+            Snackbar.make(
+                binding.root,
+                "Bluetooth pas pris en charge sur cet appareil",
+                Snackbar.LENGTH_LONG
+            ).show()
+        } else {
+            if (bluetoothAdapter?.isEnabled == true) {
+                val device = mutableListOf<String>()
+                scanDeviceWithPermissions()
+
+                togglePlayPauseAction()
+
+                Toast.makeText(this, "Bluetooth activé", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Bluetooth doit être activé", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onStop() {
+        super.onStop()
+        if (bluetoothAdapter?.isEnabled == true && allPermissionsGranted()) {
+            mscanning = false
+            bluetoothAdapter?.bluetoothLeScanner?.stopScan(leScanCallback)
+        }
+    }
+
+    private fun handleBLENotAvailable() {
+        binding.scanTitle.text = getString(R.string.ble_scan_missing)
+    }
+
+    private fun scanDeviceWithPermissions() {
+        if (allPermissionsGranted()) {
+            initToggleActions()
+        } else {
+            requestPermissionLauncher.launch(getAllPermissions())
+        }
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private fun scanBLEDevices() {
+
+        if (!mscanning) { // Stops scanning after a pre-defined scan period.
+            handler.postDelayed({
+                mscanning = false
+                bluetoothAdapter?.bluetoothLeScanner?.stopScan(leScanCallback)
+                togglePlayPauseAction()
+            }, SCAN_PERIOD)
+            mscanning = true
+            bluetoothAdapter?.bluetoothLeScanner?.startScan(leScanCallback)
+        } else {
+            mscanning = false
+            bluetoothAdapter?.bluetoothLeScanner?.stopScan(leScanCallback)
+        }
+        togglePlayPauseAction()
+    }
+
+    private val leScanCallback: ScanCallback = object : ScanCallback() {
+        @SuppressLint("NotifyDataSetChanged")
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            super.onScanResult(callbackType, result)
+            Log.d("Scan", "result : $result")
+            adapter.addDevice(result.device)
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun initToggleActions() {
         binding.scanTitle.setOnClickListener {
-            togglePlayPauseAction()
+            scanBLEDevices()
         }
 
         binding.scanPause.setOnClickListener {
-            togglePlayPauseAction()
+            scanBLEDevices()
         }
-        
+
         binding.scanList.layoutManager = LinearLayoutManager(this)
-        binding.scanList.adapter = ScanAdapter(arrayListOf("Device 1", "Device 2"))
+        adapter = ScanAdapter(arrayListOf()) {
+            val intent = Intent(this, DeviceActivity::class.java)
+            intent.putExtra("device", it)
+            startActivity(intent)
+        }
+        binding.scanList.adapter = adapter
+    }
+
+    private fun allPermissionsGranted(): Boolean {
+        val allPermissions = getAllPermissions()
+        return allPermissions.all {
+            when (it) {
+                Manifest.permission.BLUETOOTH_ADMIN,
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION -> {
+                    ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+                }
+
+                else -> false
+            }
+        }
+    }
+
+    private fun getAllPermissions(): Array<String> {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            arrayOf(
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.BLUETOOTH_SCAN
+            )
+        } else {
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        }
     }
 
     private fun togglePlayPauseAction() {
-        mScanning = !mScanning
-
-        if (mScanning) {
+        if (mscanning) {
             binding.scanTitle.text = getString(R.string.ble_scan_title_pause)
             binding.scanPause.setImageResource(R.drawable.baseline_play_arrow_24)
             binding.progressBar.isVisible = true
-        }
-        else {
+        } else {
             binding.scanTitle.text = getString(R.string.ble_scan_title_play)
             binding.scanPause.setImageResource(R.drawable.baseline_pause_24)
             binding.progressBar.isVisible = false
         }
     }
+
+    companion object {
+        private val SCAN_PERIOD: Long = 10000
+    }
+
 }
+
+
--- app/src/main/java/fr/isen/boisson/androidsmartdevice/ScanAdapter.kt
+++ app/src/main/java/fr/isen/boisson/androidsmartdevice/ScanAdapter.kt
@@ -1,5 +1,6 @@
 package fr.isen.boisson.androidsmartdevice
 
+import android.bluetooth.BluetoothDevice
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,26 +8,49 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import fr.isen.boisson.androidsmartdevice.databinding.ScanCellBinding
 
-class ScanAdapter(private val dataList: List<String>) : RecyclerView.Adapter<fr.isen.boisson.androidsmartdevice.ScanAdapter.ViewHolder>() {
-
-
-    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
-        /*val textView: TextView = itemView.findViewById(R.id.deviceName)
-        val macAddress: TextView = itemView.findViewById(R.id.macAddress)
-        val card : ConstraintLayout = itemView.findViewById(R.id.cardDevice)*/
-    }
+class ScanAdapter(var devices: ArrayList<BluetoothDevice>, var onDeviceClickListener: (BluetoothDevice) -> Unit) : RecyclerView.Adapter<ScanAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = LayoutInflater.from(parent.context).inflate(R.layout.activity_cell_adapter,parent,false)
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ScanCellBinding.inflate(inflater, parent, false)
         return ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    class ViewHolder(binding: ScanCellBinding) : RecyclerView.ViewHolder(binding.root) {
+        val deviceName = binding.deviceName
+    }
 
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.deviceName.text = devices[position].address
+        holder.itemView.setOnClickListener {
+            onDeviceClickListener(devices[position])
+        }
     }
 
     override fun getItemCount(): Int {
-        return dataList.size
+        return devices.size
+    }
+
+    fun addDevice(device: BluetoothDevice) {
+        var shouldAddDevice = true
+        devices.forEachIndexed { index, bluetoothDevice ->
+            if (bluetoothDevice.address == device.address) {
+                devices[index] = device
+                shouldAddDevice = false
+            }
+        }
+        if(shouldAddDevice) {
+            devices.add(device)
+        }
     }
 }
+
+
+/*override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = LayoutInflater.from(parent.context).inflate(R.layout.activity_cell_adapter,parent,false)
+        return ViewHolder(binding)
+    }*/
